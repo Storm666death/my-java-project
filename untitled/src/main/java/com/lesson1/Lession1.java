460e2db79c8f5fffe03cfc18a0e6bfca731535f7
Yanina <RUZETKA-BESPAROLKA@rambler.ru>
2022-05-10 23:40:15 +0300

    069

--- untitled/src/main/java/com/lesson1/Lession1.java
+++ untitled/src/main/java/com/lesson1/Lession1.java
@@ -5,7 +5,7 @@ public class Lession1 {
     public static void main(String[] args) {
         System.out.println("CompareNumbers");
         int a = 120;
-        int b = 60;
+        int b = 50;
         if (a >= b) {
             System.out.println("a>=b");
         }
